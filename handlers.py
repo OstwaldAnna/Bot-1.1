022b250b2bce0b24dcb9a3d6a3ce8d0ff1c400af
Ася Оствальд <71723290+OstwaldAnna@users.noreply.github.com>
2021-12-20 09:52:44 +0300

    Add files via upload

---
+++ handlers.py
@@ -0,0 +1,14 @@
+# Import library
+from main import bot, dp
+from config import admin_id
+from aiogram import types
+from aiogram.types import Message
+
+# Send message to admin
+#async def send_to_admin(dp):
+#	await bot.send_message(chat_id=admin_id, text="Bot start")
+# Start bot using func
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+	text = f'''Привет! {message.from_user.full_name}'''
+	await message.answer(text=text)
